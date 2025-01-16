49993619ede9046c12cb3476af71949e13bb3fa6
saad-exe <168570914+saad-exe@users.noreply.github.com>
2025-01-16 12:45:19 +0000

    datatypes learned

---
+++ basicss/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS as newer version
+
+//alert (3+3)    This doesnt work in Node.js because theres a difernet syntax in node
+
+let name="saad" //String datatype
+let age= 21     //number datatype
+let isLoggedIn= false //boolean
+
+/*
+PRIMITIVE DATATYPES
+number = 2^53
+string= ""
+boolean= true/false
+bigint
+null= Standalone value
+undefined= 
+symbol = uniqueness
+
+Object
+*/
+console.log(typeof(isLoggedIn))  //Used to find out the datatype of said variable
+console.log(typeof(null))       //Datatype is returned as an "Object" meaning null is an object
+console.log(typeof undefined)   //Its type is also undefined
