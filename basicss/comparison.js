73eeb9a47f5c07da24a4a855acb717bb368b651b
saad-exe <168570914+saad-exe@users.noreply.github.com>
2025-01-16 13:20:28 +0000

    Learned comparison operators, strict check and some unpredictable results

---
+++ basicss/comparison.js
@@ -0,0 +1,12 @@
+// console.log(2>1);
+// console.log("2">1);
+
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+//WE GET AN UNPREDICTABLE RESULT
+
+//Strict check is done using ===
+console.log("2"===2); //checks the datatype as well
+console.log("2"==2);
+
