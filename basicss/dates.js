ef621b42071adb47fd1b300521c0aa872afc7961
saad-exe <168570914+saad-exe@users.noreply.github.com>
2025-01-18 10:45:10 +0000

    Learned dates numbers and math

---
+++ basicss/dates.js
@@ -0,0 +1,22 @@
+let myDate=new Date()
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// //Many other functions too
+
+// console.log(typeof myDate);
+
+let myCreatedDate=new Date(2023,0, 23, 5, 3)    //2023 is the year, 0 is the month (January), 23 is the date, 5 is the hour and 3 is the mins
+// console.log(myCreatedDate.toLocaleString());
+ 
+let myTimeStamp=Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date();
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
---
+++ basicss/numbers.js
@@ -0,0 +1,22 @@
+// const score=20
+// const balance= new Number(100)
+// console.log(score);
+// console.log(balance);
+
+// console.log(typeof(balance.toString()));
+// console.log(balance.toFixed(3));
+
+// const hundreds=100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//******************************MATHS********************************** */
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.24));
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
