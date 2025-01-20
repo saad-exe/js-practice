4369503e920d8f40e9f03690eb86abe63228dc88
saad-exe <168570914+saad-exe@users.noreply.github.com>
2025-01-20 07:45:52 +0000

    ob

---
+++ basics2/objects.js
@@ -0,0 +1,11 @@
+//object literals
+
+const jsUser={
+    name: "saad",
+    age: 21,
+    location: "Islamabad"
+}
+console.log(jsUser);
+console.log(jsUser.location);
+console.log(jsUser["location"]);   //This way is more flexible
+
