001b34c228e6c9410359e211c111bf6c2f17cff6
saad-exe <168570914+saad-exe@users.noreply.github.com>
2025-01-20 13:04:29 +0000

    learned sum objs

--- basics2/objects.js
+++ basics2/objects.js
@@ -1,11 +1,41 @@
 //object literals
 
-const jsUser={
-    name: "saad",
-    age: 21,
-    location: "Islamabad"
-}
-console.log(jsUser);
-console.log(jsUser.location);
-console.log(jsUser["location"]);   //This way is more flexible
+// const jsUser={
+//     name: "saad",
+//     age: 21,
+//     location: "Islamabad"
+// }
+// console.log(jsUser);
+// console.log(jsUser.location);
+// console.log(jsUser["location"]);   //This way is more flexible
 
+const obj1={
+    1: "a",
+    2: "b"
+}
+const obj2={
+    3:"c",
+    4: "d"
+}
+
+obj3={...obj1, ...obj2}
+// console.log(obj3);
+
+const users=[
+    {
+        id:1,
+        name: "man"
+    },
+    {
+        id:2,
+        name: "woman"
+    },
+    {
+        id:3,
+        name:"dude"
+    }
+]
+
+users[1].email
+console.log(Object.keys(obj1));
+console.log(Object.values(obj1));
