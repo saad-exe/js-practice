6ba48e67d9e3a7704413bc874821130da9bce818
saad-exe <168570914+saad-exe@users.noreply.github.com>
2025-01-20 13:17:38 +0000

    more objecting

--- basics2/objects.js
+++ basics2/objects.js
@@ -9,33 +9,46 @@
 // console.log(jsUser.location);
 // console.log(jsUser["location"]);   //This way is more flexible
 
-const obj1={
-    1: "a",
-    2: "b"
-}
-const obj2={
-    3:"c",
-    4: "d"
-}
+// const obj1={
+//     1: "a",
+//     2: "b"
+// }
+// const obj2={
+//     3:"c",
+//     4: "d"
+// }
 
-obj3={...obj1, ...obj2}
-// console.log(obj3);
+// obj3={...obj1, ...obj2}
+// // console.log(obj3);
 
-const users=[
-    {
-        id:1,
-        name: "man"
-    },
-    {
-        id:2,
-        name: "woman"
-    },
-    {
-        id:3,
-        name:"dude"
-    }
-]
+// const users=[
+//     {
+//         id:1,
+//         name: "man"
+//     },
+//     {
+//         id:2,
+//         name: "woman"
+//     },
+//     {
+//         id:3,
+//         name:"dude"
+//     }
+// ]
 
-users[1].email
-console.log(Object.keys(obj1));
-console.log(Object.values(obj1));
+// users[1].email
+// console.log(Object.keys(obj1));
+// console.log(Object.values(obj1));
+
+//DESTRUCT OBJECTS
+const course={
+    courseName:"JS",
+    coursePrice:"2000",
+    courseInstructor:"someDude"
+};
+
+const {courseInstructor}=course;
+console.log(courseInstructor);
+
+const {courseInstructor: instructor}=course;
+console.log(instructor);
