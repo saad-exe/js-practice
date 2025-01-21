9581d4cb506e7c63af611f1d8c21db8059b687d7
saad-exe <168570914+saad-exe@users.noreply.github.com>
2025-01-21 13:22:32 +0000

    Learned functions ez asf

---
+++ basics3/functions.js
@@ -0,0 +1,14 @@
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("A");
+    console.log("D");
+}
+//sayMyName();
+
+function add2Numbers(number1, number2){
+    console.log(number1+number2);
+    return number1+number2
+}
+const newNum= add2Numbers(2,4)
+console.log(newNum);
