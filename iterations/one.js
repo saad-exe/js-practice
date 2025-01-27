4d84235ce22411f18bae09e04eaf5dcc2c9d3bbd
saad-exe <168570914+saad-exe@users.noreply.github.com>
2025-01-27 05:54:01 +0000

    hjjhhjhb

--- iterations/one.js
+++ iterations/one.js
@@ -1,4 +1,30 @@
-for (let index = 0; index < 10; index++) {
-    console.log(index);  
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);  
+    
+// }
+
+//for of
+
+// const arr=[1,3,5,6]
+// for (const val of arr) {
+//     console.log(val);
+    
+// }
+
+// const greetings= "Hello world!"
+// for (const greet of greetings) {
+//     console.log(greet);
+    
+// }
+
+//maps
+const map=new Map()
+map.set('IN','India')
+map.set('PK','Pakistan')
+map.set('IN','India')   //doent have duplicate values
+console.log(map);
+
+for (const [key,value] of map) {
+    console.log(key, ':-' , value);
     
 }
---
+++ iterations/two.js
@@ -0,0 +1,15 @@
+// const obj={
+//     js: 'Javascript',
+//     cs: 'CSS',
+//     swift: 'Swift by Apple'
+// }
+// for (const key in obj) {
+//     console.log(key);
+//     console.log(`${key} shortcut is for ${obj[key]}`);
+    
+// }
+
+const values=[5,4,2,1,76,21]
+values.forEach(function (items){
+    console.log(items);  
+})
